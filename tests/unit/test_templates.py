a99aa6918d08125e0ad18ed603cb82d54a625fd7
Igor Kaplounenko <megawidget@gmail.com>
2018-02-20 14:15:44 -0800

    fixed stringio hopefully

--- tests/unit/test_templates.py
+++ tests/unit/test_templates.py
@@ -1,5 +1,8 @@
 from contextlib import contextmanager
-from io import StringIO     # if only python3 supported cStringIO...
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from io import StringIO
 import sys
 from mlt.commands.templates import Templates
 
@@ -10,10 +13,11 @@ def catch_stdout():
     sys.stdout = caught_output = StringIO()
     yield caught_output
     sys.stdout = _stdout
+    caught_output.close()
 
 
 def test_template_list():
     templates = Templates({'template': True, 'list': True})
     with catch_stdout() as caught_output:
         templates.action()
-    assert caught_output.getvalue() is not None
+        assert caught_output.getvalue() is not None
