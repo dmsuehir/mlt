8190f0a66e13954caa6688179ac43a2392208741
Steven Robertson <steven@nervanasys.com>
2018-02-20 15:39:29 +0100

    fixed tests

--- tests/unit/test_templates.py
+++ tests/unit/test_templates.py
@@ -1,6 +1,19 @@
-from mlt.commands.templates import Template
+from contextlib import contextmanager
+from cStringIO import StringIO  # won't work in python3
+import sys
+from mlt.commands.templates import Templates
+
+
+@contextmanager
+def catch_stdout():
+    _stdout = sys.stdout
+    sys.stdout = caught_output = StringIO()
+    yield caught_output
+    sys.stdout = _stdout
 
 
 def test_template_list():
-    template = Template()
-    assert template.template_list() is not None
+    templates = Templates({'template': True, 'list': True})
+    with catch_stdout() as caught_output:
+        templates.action()
+    assert caught_output is not None
